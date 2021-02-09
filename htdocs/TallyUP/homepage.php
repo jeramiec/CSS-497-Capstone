e326f100883a3341fbbf182684f8171edd875fe8
jeramiec <44457144+jeramiec@users.noreply.github.com>
2021-02-08 21:08:33 -0800

    Add files via upload

--- htdocs/TallyUP/homepage.php
+++ htdocs/TallyUP/homepage.php
@@ -4,63 +4,93 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Homepage</title>
+		<title>Homepage dashboard</title>
+		<link rel="stylesheet" href="style.css" />
 	</head>
+	
+	
 	<body>
 		<div class="nav-container">
 			<div class="wrapper">
 				<nav>
-					<div class="logo">
-						TallyUP
+					<div class="sidebar">
+						<div class="logo">
+							<h2>TallyUP</h2>
+						</div>
+						<ul>
+							<li><a class="current" href="#"><img class="btn home" src="icons/dashboard.svg" alt="home_btn"/>homepage</a></li>
+							
+							<li><a href="#.php"><img class="btn notification" src="icons/notifications.svg" alt="notif_btn"/>
+								notifications</a>
+							</li>
+							
+							<li><a href="#"><img class="btn inventory" src="icons/inventory.svg" alt="inv_btn"/>
+								inventory</a>
+							</li>
+							
+							<li><a href="#"><img class="btn sales" src="icons/sales.svg" alt="sale_btn"/>
+								sales</a>
+							</li>
+							
+							<li><a href="#"><img class="btn expenses" src="icons/expenses.svg" alt="exp_btn"/>
+								expenses</a>
+							</li>
+							
+							<li><a href="#"><img class="btn invoices" src="icons/invoices.svg" alt="invoi_btn"/>
+								invoices</a>
+							</li>
+							
+							<li><a href="#"><img class="btn shopping" src="icons/cart.svg" alt="shop_btn"/>
+								shopping</a>
+							</li>
+							
+							<li><a href="#"><img class="btn settings" src="icons/settings.svg" alt="set_btn"/>
+								settings</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+				
+				<div class="main_container">
+					<div class="wrapper">
+						<header>
+							<div class="main_content">
+								<h1>Welcome back, (name)</h1>
+									<p>...Insert dashboard contents here...</p>
+								<a href="#">Contact me?????</a>
+							</div>
+					
+							<div class="hero-image">
+								<div class="photo-bg"></div>
+							</div>
+						</header>
+					</div>				
+				</div>
+		
+			</div>
+		</div>
+		
+		<!--
+		<div class="main-container">
+			<div class="wrapper">
+				<header>
+					<div class="main-content">
+						<h1>Welcome back, (name)</h1>
+						<p>
+							...Insert dashboard contents here...
+						</p>
+						<a href="#">
+							Contact me?????
+						</a>
 					</div>
 					
-					<ul class="nav-items">
-						<li>
-							<img class="home-btn" src="icons/dashboard.svg" alt=""/>
-							<a href="#">homepage</a>
-						</li>
-						
-						<li>
-							<img class="notifications-btn" src="icons/notifications.svg" alt=""/>
-							<a href="#">notifications</a>
-						</li>
-						
-						<li>
-							<img class="inventory-btn" src="icons/inventory.svg" alt=""/>
-							<a href="#">inventory</a>
-						</li>
-						
-						<li>
-							<img class="sales-btn" src="icons/sales.svg" alt=""/>
-							<a href="#">sales</a>
-						</li>
-						
-						<li>
-							<img class="expenses-btn" src="icons/expenses.svg" alt=""/>
-							<a href="#">expenses</a>
-						</li>
-						
-						<li>
-							<img class="invoices-btn" src="icons/invoices.svg" alt=""/>
-							<a href="#">invoices</a>
-						</li>
-						
-						<li>
-							<img class="shopping-btn" src="icons/cart.svg" alt=""/>
-							<a href="#">shopping</a>
-						</li>
-						
-						<li>
-							<img class="settings-btn" src="icons/settings.svg" alt=""/>
-							<a href="#">settings</a>
-						</li>
-					</ul>
-				</nav>
-			</div>
-			
-			
-			
+					<div class="hero-image">
+						<div class="photo-bg"></div>
+					</div>
+				</header>
+			</div>				
 		</div>
+		-->
 		
 		<script src="home.js"</script>
 	</body>
