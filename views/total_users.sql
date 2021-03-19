3036fda0307f0e74969dad9b127f83bfdc2805d0
jeramiec <44457144+jeramiec@users.noreply.github.com>
2021-03-18 21:50:50 -0700

    Delete views directory

--- views/total_users.sql
+++
@@ -1,9 +0,0 @@
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `jeramiec`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `total_users` AS
-    SELECT 
-        COUNT(0) AS `total_accounts`
-    FROM
-        `account`
