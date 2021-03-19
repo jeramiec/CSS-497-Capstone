7d8d829b668e4898e33fd4076da20f84a6864158
jeramiec <44457144+jeramiec@users.noreply.github.com>
2021-03-18 21:46:27 -0700

    Add files via upload

---
+++ sql src/system data/tallyup_table_account_type.sql
@@ -0,0 +1,9 @@
+
+--
+-- Dumping data for table `account_type`
+--
+
+INSERT INTO `account_type` (`account_type_id`, `type`, `description`) VALUES
+(0, 'Retailer', 'For storeowners who sell custom or made goods.'),
+(1, 'Reseller', 'For storeowners who resell a curated selection of goods.'),
+(2, 'Dropship', 'For storeowners who sell dropshipped goods.');
---
+++ sql src/system data/tallyup_table_category.sql
@@ -0,0 +1,9 @@
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`, `description`) VALUES
+(0, 'Merchandise', 'Items/products for company sales.'),
+(1, 'Operational expenses', 'Items/products company use.'),
+(2, 'Personal assets', 'Items/products personal use.');
